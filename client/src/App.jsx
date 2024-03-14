f7c8c8c7d6a8450ab90d01ea3ec62b8d426f98d9
ElinaTodorova <elina.todorova93@gmail.com>
2024-03-14 09:02:20 +0100

    Add Paths object

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+
+import Paths from "./paths/paths.js";
+
 import GameCreate from "./components/game-create/GameCreate.jsx";
 import GamesCatalog from "./components/games-catalog/GamesCatalog.jsx";
 import GameDetails from "./components/games-details/GameDetails.jsx";
 import GamesHome from "./components/games-home/GamesHome.jsx";
 import Header from "./components/header/Header.jsx";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div id="box">
       <Header />
       <Routes>
-        <Route path="/" element={<GamesHome />} />
-        <Route path="/games" element={ <GamesCatalog />} />
-        <Route path="/games/create" element={ <GameCreate />} />
-        <Route path="/games/:id" element={ <GameDetails />} />
+        <Route path={Paths.Home} element={<GamesHome />} />
+        <Route path={Paths.Games} element={ <GamesCatalog />} />
+        <Route path={Paths.CreateGame} element={ <GameCreate />} />
+        <Route path={Paths.Game} element={ <GameDetails />} />
       </Routes>
     </div>
   );
---
+++ client/src/paths/paths.js
@@ -0,0 +1,8 @@
+const Paths = {
+    Home : "/",
+    Games : "/games",
+    CreateGame : "/games/create",
+    Game : "/games/:id"
+}
+
+export default Paths;
