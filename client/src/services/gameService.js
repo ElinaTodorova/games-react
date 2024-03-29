8e21156da853451af08956094e1491e7d66c301a
ElinaTodorova <elina.todorova93@gmail.com>
2024-03-29 10:51:32 +0100

    Delete Game

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,26 +1,30 @@
-import GameDetails from '../components/games-details/GameDetails.jsx';
-import * as request from '../lib/request.js';
+import GameDetails from "../components/games-details/GameDetails.jsx";
+import * as request from "../lib/request.js";
 
-const baseUrl = 'http://localhost:3030/data/games';
+const baseUrl = "http://localhost:3030/data/games";
 
 export const getAll = async () => {
- const result = await request.get(baseUrl);
+  const result = await request.get(baseUrl);
 
- return result;
-}
+  return result;
+};
 
 export const create = async (data) => {
-    await request.post(baseUrl, data);
-}
+  await request.post(baseUrl, data);
+};
 
 export const getById = async (id) => {
-    const result = await request.get(`${baseUrl}/${id}`);
+  const result = await request.get(`${baseUrl}/${id}`);
 
-    return result;
-}
+  return result;
+};
 
 export const edit = async (gameId, gameData) => {
-    const result = await request.put(`${baseUrl}/${gameId}`, gameData);
+  const result = await request.put(`${baseUrl}/${gameId}`, gameData);
 
-    return result;
-}
+  return result;
+};
+
+export const deleteGame = async (gameId) => {
+  await request.remove(`${baseUrl}/${gameId}`);
+};
